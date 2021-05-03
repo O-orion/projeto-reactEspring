6f8294e590e3d6b2cb678a52d9551d2c066cf521
Lucas Viana <lucas.lvtorres@gmail.com>
2021-05-03 16:30:23 -0400

    Projetos criado - Jesus Cristo é o senhor

---
+++ backend/src/test/java/JesusCristoEoSenhor/JsVendas/JsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package JesusCristoEoSenhor.JsVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
